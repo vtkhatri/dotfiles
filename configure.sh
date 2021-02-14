05f0dfa2379a03529f9c34d0e1beb4295dd9532f
Viraj Khatri <virajkhatri@gmail.com>
2021-02-14 10:57:03 +0530

    Setting up config script

---
+++ configure.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+user=`whoami`
+if [ "$user" == "root" ]
+then
+	echo "Why are you running (as root)?"
+	exit
+fi
+
+
+if [ ! -d "~/.vim" ]
+then
+	echo "~/.vim not found, making parent, backup and undo dirs"
+	mkdir -p ~/.vim/bkps ~/.vim/undos
+fi
+
+if [ -f /home/$user/.vimrc ]
+then
+	echo "moving ~/.vimrc to ~/.vim/old_vimrc"
+	mv /home/$user/.vimrc /home/$user/.vim/old_vimrc
+fi
+
+if [ -f /home/$user/.vim/vimrc ]
+then
+	if [ ! -L /home/$user/.vim/vimrc ]
+	then
+		echo "~/.vim/vimrc not a symlink, moving file and linking"
+		mv /home/$user/.vim/vimrc /home/$user/.vim/old_vimrc
+		ln -s $PWD/vimrc /home/$user/.vim/vimrc
+	else
+		echo "~/.vim/vimrc is already a symlink, updating"
+		rm /home/$user/.vim/vimrc
+		ln -s $PWD/vimrc /home/$user/.vim/vimrc
+	fi
+else
+	ln -s $PWD/vimrc /home/$user/.vim/vimrc
+fi
+
+if [ -f /home/$user/.tmux.conf ]
+then
+	if [ ! -L /home/$user/.tmux.conf ]
+	then
+		echo "~/.tmux.conf not a symlink, moving file and linking"
+		mv /home/$user/.tmux.conf /home/$user/.old_tmux.conf
+		ln -s $PWD/.tmux.conf /home/$user/.tmux.conf
+	else
+		echo "~/.tmux.conf is already a symlink, updating"
+		rm /home/$user/.tmux.conf
+		ln -s $PWD/.tmux.conf /home/$user/.tmux.conf
+	fi
+else
+	ln -s $PWD/.tmux.conf /home/$user/.tmux.conf
+fi
