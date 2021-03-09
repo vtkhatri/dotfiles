d3dbb1bea62e96ddcab8647fe9af85149fc85e7d
Viraj Khatri <virajkhatri@gmail.com>
2021-03-09 22:28:34 +0530

    Writing function to reduce re-used code

--- configure.sh
+++ configure.sh
@@ -7,9 +7,36 @@ then
 	exit
 fi
 
-##############
-# vim config #
-##############
+create_links ()
+{
+	if [ "$#" -ne 2 ]
+	then
+		echo "create_links used wrong for "$@
+		echo "usage: create_links file full_path_to_link"
+		return 1
+	fi
+
+	current_dir_file=$1
+	link_path=$2
+	if [ -f $link_path ]
+	then
+		if [ ! -L $link_path ]
+		then
+			echo "$link_path not a symlink, moving file and linking"
+			mv $link_path ${link_path}_old
+			ln -s $PWD/$current_dir_file $link_path
+		else
+			echo "$link_path is already a symlink, updating"
+			rm $link_path
+			ln -s $PWD/$current_dir_file $link_path
+		fi
+	else
+		echo ln -s $PWD/$current_dir_file $link_path
+	fi
+}
+
+# vim config
+
 if [ ! -d /home/$user/.vim ]
 then
 	echo "~/.vim not found, making parent, backup and undo dirs"
@@ -22,94 +49,21 @@ then
 	mv /home/$user/.vimrc /home/$user/.vim/old_vimrc
 fi
 
-if [ -f /home/$user/.vim/vimrc ]
-then
-	if [ ! -L /home/$user/.vim/vimrc ]
-	then
-		echo "~/.vim/vimrc not a symlink, moving file and linking"
-		mv /home/$user/.vim/vimrc /home/$user/.vim/old_vimrc
-		ln -s $PWD/vimrc /home/$user/.vim/vimrc
-	else
-		echo "~/.vim/vimrc is already a symlink, updating"
-		rm /home/$user/.vim/vimrc
-		ln -s $PWD/vimrc /home/$user/.vim/vimrc
-	fi
-else
-	ln -s $PWD/vimrc /home/$user/.vim/vimrc
-fi
+create_links vimrc /home/$user/.vim/vimrc
 
-###############
-# tmux config #
-###############
-if [ -f /home/$user/.tmux.conf ]
-then
-	if [ ! -L /home/$user/.tmux.conf ]
-	then
-		echo "~/.tmux.conf not a symlink, moving file and linking"
-		mv /home/$user/.tmux.conf /home/$user/.old_tmux.conf
-		ln -s $PWD/tmux.conf /home/$user/.tmux.conf
-	else
-		echo "~/.tmux.conf is already a symlink, updating"
-		rm /home/$user/.tmux.conf
-		ln -s $PWD/tmux.conf /home/$user/.tmux.conf
-	fi
-else
-	ln -s $PWD/tmux.conf /home/$user/.tmux.conf
-fi
+# tmux config
 
-##############
-# bash setup #
-##############
-if [ -f /home/$user/.bash_profile ]
-then
-	if [ ! -L /home/$user/.bash_profile ]
-	then
-		echo "~/.bashrc not a symlink, moving file and linking"
-		mv /home/$user/.bash_profile /home/$user/.old_bash_profile
-		ln -s $PWD/bash_profile /home/$user/.bash_profile
-	else
-		echo "~/.bashrc is already a symlink, updating"
-		rm /home/$user/.bash_profile
-		ln -s $PWD/bash_profile /home/$user/.bash_profile
-	fi
-else
-	ln -s $PWD/bash_profile /home/$user/.bash_profile
-fi
+create_links tmux.conf /home/$user/.tmux.conf
 
-if [ -f /home/$user/.bashrc ]
-then
-	if [ ! -L /home/$user/.bashrc ]
-	then
-		echo "~/.bashrc not a symlink, moving file and linking"
-		mv /home/$user/.bashrc /home/$user/.old_bashrc
-		ln -s $PWD/bashrc /home/$user/.bashrc
-	else
-		echo "~/.bashrc is already a symlink, updating"
-		rm /home/$user/.bashrc
-		ln -s $PWD/bashrc /home/$user/.bashrc
-	fi
-else
-	ln -s $PWD/bashrc /home/$user/.bashrc
-fi
+# bash setup
+
+create_links bash_profile /home/$user/.bash_profile
+create_links bashrc /home/$user/.bashrc
 
 if [ ! -d /home/$user/.config ]
 then
 	echo "~/.config not found, making directory"
 	ln -s $PWD/exports_bash /home/$user/.config/exports_bash
 else
-	if [ -f /home/$user/.config/exports_bash ]
-	then
-		if [ ! -L /home/$user/.config/exports_bash ]
-		then
-			echo "~/.config/exports_bash not a symlink, moving file and linking"
-			mv /home/$user/.config/exports_bash /home/$user/.config/old_exports_bash
-			ln -s $PWD/exports_bash /home/$user/.config/exports_bash
-		else
-			echo "~/.config/exports_bash is already a symlink, updating"
-			rm /home/$user/.config/exports_bash
-			ln -s $PWD/exports_bash /home/$user/.config/exports_bash
-		fi
-	else
-		ln -s $PWD/exports_bash /home/$user/.config/exports_bash
-	fi
+	create_links exports_bash /home/$user/.config/exports_bash
 fi
