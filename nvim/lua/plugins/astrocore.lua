cf69a0ce1baaf64596da0a70908bb42bc9aa2168
vtkhatri <virajkhatri@gmail.com>
2025-03-31 13:44:09 -0700

    nvim wildmode

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -32,6 +32,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        wildmode = "longest,full", -- better wildmode completion
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
