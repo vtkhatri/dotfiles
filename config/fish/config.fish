22b4e9d21805143d2fec560c18504b590a58f1e6
vtkhatri <virajkhatri@gmail.com>
2025-08-14 18:25:55 -0700

    local config for fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -15,3 +15,7 @@ fish_add_path $HOME/.cargo/bin
 if test -f /$HOME/.vim/pack/fzf/start/fzf/shell/key-bindings.fish;
 	source /$HOME/.vim/pack/fzf/start/fzf/shell/key-bindings.fish
 end
+
+if test -f /$HOME/.config/exports.local.fish;
+	source $HOME/.config/exports.local.fish
+end
